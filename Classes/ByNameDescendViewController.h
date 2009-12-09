48e6dab214883da0da3bfbf5cb7aea690fc71f52
beepscore <support@beepscore.com>
2009-12-09 08:44:40 -0800

    Add many view controllers.  Ref Dudney sec 7.4

---
+++ Classes/ByNameDescendViewController.h
@@ -0,0 +1,16 @@
+//
+//  ByNameDescendViewController.h
+//  States
+//
+//  Created by Steve Baker on 12/9/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ByNameDescendViewController : UITableViewController {
+
+}
+
+@end
---
+++ Classes/ByPopAscendViewController.h
@@ -0,0 +1,16 @@
+//
+//  ByPopAscendViewController.h
+//  States
+//
+//  Created by Steve Baker on 12/9/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ByPopAscendViewController : UITableViewController {
+
+}
+
+@end
